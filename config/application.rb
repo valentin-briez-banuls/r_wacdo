5dc1f595cf58215e48dd5504ffd939d66463a0b2
valentin <val66450@gmail.com>
2025-06-03 09:18:09 +0200

    UPDATE - Prod Dockerfile

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module Wacdo
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.action_cable.enabled = false
 
     # Configuration for the application, engines, and railties goes here.
     #
