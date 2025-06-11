7aa39d3ec9907c2083fbbeceb391beeb06c7b2e2
valentin <val66450@gmail.com>
2025-06-11 14:22:45 +0200

    UPDATE - README.md

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,9 +4,8 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require 'rspec/rails'
-require 'active_record/railtie'  # ✅ Ajoute cette ligne si tu as l'erreur
+require 'active_record/railtie'
 
-# Assure-toi que les migrations sont à jour
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -14,11 +13,9 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
-  # ActiveRecord est utilisé pour gérer les fixtures et DB de test
   config.use_active_record = true
 
-  # Spécifie le chemin des fixtures (si tu en utilises)
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
 
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-# spec/spec_helper.rb
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
